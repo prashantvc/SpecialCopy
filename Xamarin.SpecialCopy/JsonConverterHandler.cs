032e959cd356c491f18144a8d96445936866c468
Prashant Cholachagudda <pvc@outlook.com>
2014-04-30 22:36:29 +0530

    Initial commit

---
+++ Xamarin.SpecialCopy/JsonConverterHandler.cs
@@ -0,0 +1,72 @@
+﻿using MonoDevelop.Components.Commands;
+using System;
+using MonoDevelop.Ide;
+using Mono.TextEditor;
+using MonoDevelop.Ide.Gui;
+using Gtk;
+using Xamasoft.JsonClassGenerator;
+using Xamasoft.JsonClassGenerator.CodeWriters;
+using System.IO;
+
+namespace SpecialCopy.JsonConverter
+{
+	class JsonConverterHandler : CommandHandler
+	{
+		void PasteJson (Clipboard c, string text)
+		{
+			if (string.IsNullOrEmpty (text)) {
+				InsertCSharp (string.Empty);
+				return;
+			}
+
+			var gen = new JsonClassGenerator { 
+				Example = text, 
+				MainClass = "RootObject",
+				UseProperties = true, 
+				CodeWriter = new CSharpCodeWriter () 
+			};
+
+			try {
+				using (var sw = new StringWriter ()) {
+					gen.OutputStream = sw;
+					gen.GenerateClasses ();
+					sw.Flush ();
+					var generatedString = sw.ToString ();
+					InsertCSharp (generatedString);
+				}
+			} catch (Exception ex) {
+				Console.WriteLine (ex.Message);
+				MessageService.ShowWarning (string.Format ("Invalid JSON: {0}", ex.Message));
+			}
+
+			gen = null;
+		}
+
+		void InsertCSharp (string text)
+		{
+			var doc = IdeApp.Workbench.ActiveDocument;
+			var textEditorData = doc.GetContent<ITextEditorDataProvider> ().GetTextEditorData ();
+
+			textEditorData.InsertAtCaret (text);
+		}
+
+		protected override void Run ()
+		{
+			var clipboard = Clipboard.Get (Gdk.Selection.Clipboard);
+			clipboard.RequestText (PasteJson);
+		}
+
+		protected override void Update (CommandInfo info)
+		{
+			Console.WriteLine (info);
+
+			var doc = IdeApp.Workbench.ActiveDocument;
+			info.Enabled = doc != null && doc.GetContent<ITextEditorDataProvider> () != null;
+		}
+	}
+
+	public enum JsonConverterCommand
+	{
+		PasteJson
+	}
+}
