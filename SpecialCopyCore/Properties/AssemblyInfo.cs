9675c4294351c3d8e485885b83d14a9efb27311c
Prashant Cholachagudda <pvc@outlook.com>
2014-12-24 23:25:45 +0530

    Using GTK project template

--- SpecialCopyCore/Properties/AssemblyInfo.cs
+++ SpecialCopyCore/Properties/AssemblyInfo.cs
@@ -3,7 +3,8 @@ using System.Runtime.CompilerServices;
 
 // Information about this assembly is defined by the following attributes.
 // Change them to the values specific to your project.
-[assembly: AssemblyTitle ("SpecialCopy")]
+
+[assembly: AssemblyTitle ("SpecialCopyCore")]
 [assembly: AssemblyDescription ("")]
 [assembly: AssemblyConfiguration ("")]
 [assembly: AssemblyCompany ("")]
@@ -11,12 +12,16 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyCopyright ("Prashant Cholachagudda")]
 [assembly: AssemblyTrademark ("")]
 [assembly: AssemblyCulture ("")]
+
 // The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
+
 [assembly: AssemblyVersion ("1.0.*")]
+
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
+
 //[assembly: AssemblyDelaySign(false)]
 //[assembly: AssemblyKeyFile("")]
 
