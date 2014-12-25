8088032b04ef567c346a451e1b70ead04fa4a326
Prashant Cholachagudda <pvc@outlook.com>
2014-12-25 11:05:47 +0530

    Add default values

--- SpecialCopyCore/PropertyList.cs
+++ SpecialCopyCore/PropertyList.cs
@@ -6,17 +6,17 @@ namespace SpecialCopy
 	public static class PropertyList
 	{
 		public static bool UsePascalCase {
-			get{ return PropertyService.Get<bool> ("SpecialCopy.UsePascalCase"); }
+			get{ return PropertyService.Get<bool> ("SpecialCopy.UsePascalCase", true); }
 			set{ PropertyService.Set ("SpecialCopy.UsePascalCase", value); }
 		}
 
 		public static bool UseProperties {
-			get{ return PropertyService.Get<bool> ("SpecialCopy.UseProperties"); }
+			get{ return PropertyService.Get<bool> ("SpecialCopy.UseProperties", true); }
 			set{ PropertyService.Set ("SpecialCopy.UseProperties", value); }
 		}
 
 		public static bool InternalClass {
-			get{ return PropertyService.Get<bool> ("SpecialCopy.InternalClass"); }
+			get{ return PropertyService.Get<bool> ("SpecialCopy.InternalClass", true); }
 			set{ PropertyService.Set ("SpecialCopy.InternalClass", value); }
 		}
 	}
